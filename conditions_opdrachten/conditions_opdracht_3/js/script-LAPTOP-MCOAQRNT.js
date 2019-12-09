96dc936c537e26be887567241f9b86b72ac9d898
mohamednassarelkharroubi <2105628@talnet.nl>
2019-12-09 13:45:11 +0100

    4

---
+++ conditions_opdrachten/conditions_opdracht_3/js/script-LAPTOP-MCOAQRNT.js
@@ -0,0 +1,3 @@
+if(getSaldo>=25) {
+    document.getElementById("getSaldo").innerHTML="Je hebt voldoende saldo om 25$ op te nemen.";
+}
--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,9 @@
-//javascript code
+function getSaldo()
+{
+var getSaldo=500;
+
+
+    if(getSaldo>=500) {
+        document.getElementById("getSaldo").innerHTML="bonus 0,2 rente.";
+    }
+}
