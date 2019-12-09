0438cc9a18bdc6dac2ecd4747818080f89b97285
mohamednassarelkharroubi <2105628@talnet.nl>
2019-12-09 13:23:26 +0100

    3

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,12 @@
-//javascript code
+function getSaldo()
+{
+var getSaldo=35;
+
+
+    if(getSaldo>=25) {
+        document.getElementById("getSaldo").innerHTML="Je hebt voldoende saldo om 25$ op te nemen.";
+    }
+    if(getSaldo<=25) {
+        document.getElementById("getSaldo").innerHTML="je hebt weinig saldo";
+    }
+}
